36276466fd43a4f47a0714b1899e777a14a2677c
Parker Siddall <parker.siddall@gmail.com>
2021-03-23 13:22:05 +0100

    adding exercises 1.3 - 1.5

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Full Stack Open 2021
 Full Stack Open is a MOOC offered through the University of Helsinki that covers the fundamentals of modern web development. 
 
-The course focuses on creating single page applications which use React.js on the front end and Node.js on the backend. MondoDB, TypeScript, and GraphQL are also covered in the course. 
+The course focuses on creating single page applications which use React.js on the front end and Node.js on the backend. MongoDB, TypeScript, and GraphQL are also covered in the course. 
 
 This repository contains the assignments for each part of the course. Within each subdirectory you will find another README file with descriptions of the objectives for each task. 
 
--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -12,7 +12,13 @@ const Content = (props) => {
   return (
     <div>
       <p>
-        {props.part} {props.exercises}
+        {props.parts[0].name} {props.parts[0].exercizes}
+      </p>
+      <p>
+        {props.parts[1].name} {props.parts[1].exercizes}
+      </p>
+      <p>
+        {props.parts[2].name} {props.parts[2].exercizes}
       </p>
     </div>
   )
@@ -21,27 +27,35 @@ const Content = (props) => {
 const Total = (props) => {
   return (
     <div>
-      <p>Number of exercises {props.e1 + props.e2 + props.e3}</p>
+      <p>Number of exercises {props.parts[0].exercizes + props.parts[1].exercizes + props.parts[2].exercizes}</p>
     </div>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercizes: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercizes: 7
+      },
+      {
+        name: 'State of a component',
+        exercizes: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
-      <Total e1={exercises1} e2={exercises2} e3={exercises3} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
