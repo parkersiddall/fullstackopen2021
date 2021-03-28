a089957e0264ffef4314aa3659c2f112a343ec36
Parker Siddall <parker.siddall@gmail.com>
2021-03-28 15:51:41 +0200

    adding excersise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import AllPeople from './components/AllPeople'
 import FilteredPeople from './components/FilteredPeople'
 import FilterForm from './components/FilterForm'
@@ -7,15 +8,23 @@ import InputForm from './components/InputForm'
 const App = () => {
 
   // states
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [ persons, setPersons ] = useState([]) 
   const [ newName, setNewName ] = useState('Insert a name...')
   const [ newNumber, setNewNumber ] = useState('555-555-5555')
   const [ filter, setFilter ] = useState('')
+
+  // effects
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+  console.log('render', persons.length, 'notes')
+
 
   const filterNames = persons.filter((person) => {
     return person.name.toLowerCase().includes(filter.toLowerCase())
