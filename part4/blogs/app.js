01835aaf7c68bc30b5ce78f906f20b303615a053
Parker Siddall <parker.siddall@gmail.com>
2021-04-12 22:15:20 +0200

    adding excercise 4.2

---
+++ part4/blogs/app.js
@@ -0,0 +1,19 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+logger.info('connecting to', config.MONGO_URL)
+
+const mongoUrl = process.env.MONGO_URL
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
