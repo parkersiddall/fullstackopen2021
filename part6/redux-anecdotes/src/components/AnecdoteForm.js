897697b56855daacf805a87018a712a03fbc2f4b
Parker Siddall <parker.siddall@gmail.com>
2021-05-10 21:52:22 +0200

    adding exercise 6.14

--- part6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,21 +1,23 @@
 import { prepareCreateAnecdote } from '../reducers/anecdoteReducer'
 import { useSelector, useDispatch } from 'react-redux'
 import { createMessage, clearMessage } from '../reducers/messageReducer'
+import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = (props) => {
     const dispatch = useDispatch()
 
-    const createAnecdote = (event) => {
+    const createAnecdote = async (event) => {
         event.preventDefault()
         const content = event.target.anecdoteInput.value
         event.target.anecdoteInput.value = ''
+
+        const newAnecdote = await anecdoteService.createNew(content)
+        dispatch(prepareCreateAnecdote(content))
 
         dispatch(createMessage('Anecdote added successfully!'))
         setTimeout(() => {
           dispatch(clearMessage())
         }, 5000)
-  
-        dispatch(prepareCreateAnecdote(content))
       }
 
     return(
