f1d88087f319ea1059e219dd489838f3bab72f3a
Parker Siddall <parker.siddall@gmail.com>
2021-05-16 11:03:04 +0200

    adding exercise 7.5

--- part7/routed-anecdotes/src/hooks.js
+++ part7/routed-anecdotes/src/hooks.js
@@ -6,10 +6,12 @@ export const useField = (name) => {
     const onChange = (event) => {
       setValue(event.target.value)
     }
+    const reset = () => setValue('')
   
     return {
       name,
       value,
-      onChange
+      onChange,
+      reset
     }
   }
